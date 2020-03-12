73b3ae5156233de3b414c559234e5caef66e2186
youzehong <youzehong@9fbank.com.cn>
2020-03-12 16:00:50 +0800

    选择地址弹框

--- pages/mine/address-add.vue
+++ pages/mine/address-add.vue
@@ -1,6 +1,9 @@
 <template>
-  <view>
-    <van-area :area-list="areaList" value="110101" />
+  <view class="body">
+    <button type="primary" @click="onSeletAddress">选择地址</button>
+    <van-action-sheet :show="show" :round="round" :close-on-click-overlay="closeOverlay">
+      <van-area :area-list="areaList" :columns-placeholder="columnsPlaceholder" @confirm="onAreaConfirm" @cancel="onAreaCancel" />
+    </van-action-sheet>
   </view>
 </template>
 
@@ -9,7 +12,11 @@ import areaList from './utils/area.js';
 export default {
   data() {
     return {
-      areaList : {}
+      areaList: {},
+      show: false,
+      round: false,
+      closeOverlay: true,
+      columnsPlaceholder: ['请选择', '请选择', '请选择']
     };
   },
   onLoad(option) {
@@ -17,8 +24,16 @@ export default {
     this.areaList = areaList;
   },
   methods: {
+    onSeletAddress() {
+      this.show = true;
+    },
+    onAreaConfirm() {
+      this.show = false;
+    },
+    onAreaCancel() {
+      this.show = false;
+    },
     onSubmit() {
-      
       // this.submitLoading = true;
       // setTimeout(() => {
       //   this.submitLoading = false;
@@ -28,4 +43,15 @@ export default {
 };
 </script>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.body {
+  & ::v-deep {
+    .van-picker__cancel {
+      color: #494949;
+    }
+    .van-picker__confirm {
+      color: #f50000;
+    }
+  }
+}
+</style>
