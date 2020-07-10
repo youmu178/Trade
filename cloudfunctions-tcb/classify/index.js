39b66c6352eb0529d292022cfdcfb7173f7aed42
youzehong <youzehong@9fbank.com.cn>
2020-07-10 18:26:36 +0800

    no message

--- cloudfunctions-tcb/classify/index.js
+++ cloudfunctions-tcb/classify/index.js
@@ -3,9 +3,5 @@ exports.main = async (event, context) => {
   //event为客户端上传的参数
   console.log('event : ' + event)
   //返回数据给客户端
-  // return event
-  return {
-      status: 1,
-      msg: '成功'
-    }
+  return event
 };
